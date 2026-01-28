c463898c0688220028b11687bafbf51defa30ca6
Rhiswana <rhiswanabegam@gmail.com>
2026-01-28 05:51:54 +0000

    dashboard added

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { DashboardComponent } from './components/Dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', component: RecipeListComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'recipes', component: RecipeListComponent },
   { path: 'add', component: RecipeFormComponent },
   { path: 'edit/:id', component: RecipeFormComponent }
 ];
